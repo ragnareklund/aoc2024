f8f4bb4868b7d18d65fc85565c5b816a1ab53c71
Ragnar Eklund <ragnar.eklund@gmail.com>
2024-12-06 17:46:15 +0100

    Update 2.js

--- day4/2.js
+++ day4/2.js
@@ -7,18 +7,6 @@ var minY = 0;
 var maxX = 0;
 var minX = 0;
 
-const N  = [0,1];
-const NE = [1,1];
-const E  = [1,0];
-const SE = [1,-1];
-const S  = [0,-1];
-const SW = [-1,-1];
-const W  = [-1,0];
-const NW = [-1,1];
-
-
-const directions = [NE,SE,SW,NW]
-
 const rl = readline.createInterface({
   input: fs.createReadStream('day4/input.txt'),
   crlfDelay: Infinity,
@@ -37,10 +25,10 @@ const searcher = (x, y) => {
   let found = 0;
   if (input[y][x] !== 'A') return 0;
   
-  let [neX, neY] = [(x + NE[0]), (y + NE[1])];
-  let [seX, seY] = [(x + SE[0]), (y + SE[1])];
-  let [swX, swY] = [(x + SW[0]), (y + SW[1])];
-  let [nwX, nwY] = [(x + NW[0]), (y + NW[1])];
+  let [neX, neY] = [(x + 1), (y + 1)];
+  let [seX, seY] = [(x + 1), (y - 1)];
+  let [swX, swY] = [(x - 1), (y - 1)];
+  let [nwX, nwY] = [(x - 1), (y + 1)];
 
   if([[neX, neY], [seX, seY], [swX, swY], [nwX, nwY]].some(outOfBounds)) return 0;
 
@@ -58,7 +46,6 @@ const searcher = (x, y) => {
 rl.on('close', () => {
   maxY = input.length;
   maxX = input[0].length;
-  console.log(input);
   let found = 0;
   for(let y=0; y<input.length; y++) {
     for(let x=0; x<input[y].length; x++) {
